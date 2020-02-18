45deba29ff7a8e4479bcfe7515ccc02caaac048c
Reza <rezaxxxx02@gmail.com>
2020-02-18 23:49:42 +0700

    Create tester for speech recognition

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function App() {  
+
+  const [staListen, setStaListen] = useState(false);
+
+  const [intern, setIntern] = useState("");
+
+  const [final, setFinal] = useState("");
+
+  const startListen = () => {
+    setStaListen(true);    
+  }
+
+  const stopListen = () => {
+    setStaListen(false);
+  }
+
+  useEffect(()=>{
+
+    const Listen = window.webkitSpeechRecognition || window.SpeechRecognition;  
+    const recog = new Listen();
+
+    recog.continous = true;
+    recog.interimResults = true;
+    recog.lang = 'id-ID';
+    
+    if(staListen === true){
+
+      console.log("Start");
+
+      recog.start();      
+
+      recog.onend = () => {
+        setStaListen(false);
+      }
+
+    }
+
+    else if(staListen === false){
+
+      console.log("Stop");
+
+      recog.stop();              
+
+    }
+
+    recog.onstart = () => {
+      console.log("Listening");
+    }
+
+    let finalTranscript = "";
+
+    recog.onresult = event => {
+
+      console.log(event.results);
+
+      let interimTranscript = "";
+
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        
+        const transcript = event.results[i][0].transcript;
+        
+        if (event.results[i].isFinal) finalTranscript += transcript + ' ';
+        else interimTranscript += transcript;
+
+      }
+
+      setIntern(interimTranscript);
+
+      setFinal(finalTranscript);      
+
+
+    }      
+
+    recog.onerror = (err) => {
+
+      console.log(err);      
+
+      console.log("Error in recognition");      
+
+      setStaListen(false);
+
+    }
+
+  },[staListen]);
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={startListen}>Action for Listening</button>
+      <br></br> 
+      <span id='interim' >{intern}</span>
+      <br></br>
+      <span id='final' >{final}</span>
+      <br></br>
+      <button onClick={stopListen}>Action for stop Listening</button>
     </div>
   );
 }
